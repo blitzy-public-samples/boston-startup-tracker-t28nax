29e16b8be4fb830403391f9243c6c314e3c54ba3
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-20 01:30:06 +0530

    Add public-samples/boston-startup-tracker-t28nax/src/frontend/store/actions/index.ts

---
+++ src/frontend/store/actions/index.ts
@@ -0,0 +1,15 @@
+// Import all action creators from their respective files
+import * as startupActions from './startupActions';
+import * as investorActions from './investorActions';
+import * as userActions from './userActions';
+import * as searchActions from './searchActions';
+import * as authActions from './authActions';
+
+// Export all action creators as named exports
+export {
+  startupActions,
+  investorActions,
+  userActions,
+  searchActions,
+  authActions
+};
